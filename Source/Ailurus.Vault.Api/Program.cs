a08130e79a2c8e9d0f04ce75470025b82c1a23aa
Joseph Tindall <77989761+josephtindall@users.noreply.github.com>
2025-05-07 17:04:39 -0400

    Initial Api project

---
+++ Source/Ailurus.Vault.Api/Program.cs
@@ -0,0 +1,21 @@
+namespace Ailurus.Vault.Api;
+
+public abstract class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddAuthorization();
+        builder.Services.AddOpenApi();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment()) app.MapOpenApi();
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+
+        app.Run();
+    }
+}
